f14d052d24682296548e4e987c783c165cbb034c
Ilya <i.oberemok777@gmail.com>
2022-03-11 10:48:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# TODO: Create a youtube viedo downloader with a simple GUI interface.
+
+# imports
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import pytube
+import os
